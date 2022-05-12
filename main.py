af5d41c84588a0d860a5967e2291f92b639a438c
RazvanCiulica96 <razvan.ciulica@yahoo.com>
2022-05-12 13:25:52 +0300

    Initial commit

---
+++ main.py
@@ -0,0 +1,9 @@
+#1. Create greeting
+print("Welcome to the Band Name generator.")
+#2. Ask the user in which city did they grow up in
+city = input("Which city did you grow up in?\n")
+#3. Ask the user for the name of it's pet
+pet = input("Which is the name of your pet\n")
+#4. Combine the name of their city and pet and show them their rabbit band name.
+print("Your name could be " + city + " " + pet)
+#5. Make sure the imput cursor shows on a new line.
